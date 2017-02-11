bf0fc0020bdd2ce5dffd964c62bb9654c4ee68b8
Zed Spencer-Milnes <zed@gingergeek.co.uk>
2017-02-11 22:31:46 +0000

    Add vis usage, taken from their demo site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {VisEdges, VisNetworkData, VisNetworkOptions, VisNetworkService, VisNodes} from 'ng2-vis';
+
+
+class ExampleNetworkData implements VisNetworkData {
+  public nodes: VisNodes;
+  public edges: VisEdges;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit, OnDestroy {
+
+  public visNetwork = 'networkId1';
+  public visNetworkData: ExampleNetworkData;
+  public visNetworkOptions: VisNetworkOptions;
+
+  public constructor(private visNetworkService: VisNetworkService) { }
+
+  public addNode(): void {
+    const newId = this.visNetworkData.nodes.getLength() + 1;
+    this.visNetworkData.nodes.add({ id: newId.toString(), label: 'Node ' + newId });
+    this.visNetworkService.fit(this.visNetwork);
+  }
+
+  public networkInitialized(): void {
+    // now we can use the service to register on events
+    this.visNetworkService.on(this.visNetwork, 'click');
+
+    // open your console/dev tools to see the click params
+    this.visNetworkService.click
+      .subscribe((eventData: any[]) => {
+        if (eventData[0] === this.visNetwork) {
+          console.log(eventData[1]);
+        }
+      });
+  }
+
+  public ngOnInit(): void {
+    const nodes = new VisNodes([
+      { id: '1', label: 'Node 1' },
+      { id: '2', label: 'Node 2' },
+      { id: '3', label: 'Node 3' },
+      { id: '4', label: 'Node 4' },
+      { id: '5', label: 'Node 5', title: 'Title of Node 5' }]);
+
+    const edges = new VisEdges([
+      { from: '1', to: '3' },
+      { from: '1', to: '2' },
+      { from: '2', to: '4' },
+      { from: '2', to: '5' }]);
+
+    this.visNetworkData = {
+      nodes: nodes,
+      edges: edges
+    };
+
+    this.visNetworkOptions = {};
+  }
+
+  public ngOnDestroy(): void {
+    this.visNetworkService.off(this.visNetwork, 'click');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { VisModule } from 'ng2-vis';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    VisModule
   ],
   providers: [],
   bootstrap: [AppComponent]
